8120f7f993de0c614bbc7613e5794716303656fb
Nihal Ismail <Nihal Ismail>
2017-10-01 12:55:53 +0530

    Feature scaled values

--- Classification Template.py
+++ Classification Template.py
@@ -9,4 +9,13 @@ Created on Sun Oct  1 12:47:03 2017
 import numpy as np
 import pandas as pd
 
+#Importing dataset
 dataset=pd.read_csv("Social_Network_Ads.csv")
+x=dataset.iloc[:,2:4].values
+y=dataset.iloc[:,-1].values
+
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+scaler=StandardScaler()
+x=scaler.fit_transform(x)
+
